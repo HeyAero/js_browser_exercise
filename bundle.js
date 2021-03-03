1c15733cfb98255a0a9eeb55cb3be9b7e9f4da95
HeyAero <aeroyts@icloud.com>
2021-03-03 12:58:44 +0000

    add functionality for all exercises

--- bundle.js
+++ bundle.js
@@ -11,9 +11,43 @@ function returnColor() {
   main.style.backgroundColor = 'white';
 }
 
+function changeWords() {
+  console.log('Changing Text');
+  let heading2 = document.getElementById('heading-2');
+  heading2.textContent = 'Hello there!';
+}
+
+function addParagraph(e) {
+  let main = document.querySelector('main');
+  let para = document.createElement('p');
+  let count = document.querySelectorAll('p');
+  para.id = `para-${count.length + 1}`;
+  para.textContent = e.key;
+  main.appendChild(para);
+}
+
+function changeStyling(nav) {
+  let para1 = document.getElementById('para-1');
+  let para2 = document.getElementById('para-2');
+  let para3 = document.getElementById('para-3');
+  if (nav === 'nav-1') {
+    para1.style.fontSize = '2rem';
+    para1.style.fontWeight = 'bold';
+  } else if (nav === 'nav-2') {
+    para2.style.fontSize = '2rem';
+    para2.style.fontWeight = 'bold';
+  } else {
+    para3.style.fontSize = '2rem';
+    para3.style.fontWeight = 'bold';
+  }
+}
+
 module.exports = {
   switchColor,
   returnColor,
+  changeWords,
+  addParagraph,
+  changeStyling,
 };
 
 },{}],2:[function(require,module,exports){
@@ -21,10 +55,22 @@ const events = require('./events');
 
 function initBindings() {
   let header1 = document.getElementById('heading-1');
+  let heading2 = document.getElementById('heading-2');
+  let nav1 = document.getElementById('nav-1');
+  let nav2 = document.getElementById('nav-2');
+  let nav3 = document.getElementById('nav-3');
   console.log(header1);
 
   header1.addEventListener('click', events.switchColor);
   header1.addEventListener('mouseout', events.returnColor);
+
+  heading2.addEventListener('mouseover', events.changeWords);
+
+  document.addEventListener('keydown', events.addParagraph);
+
+  nav1.addEventListener('click', () => events.changeStyling('nav-1'));
+  nav2.addEventListener('click', () => events.changeStyling('nav-2'));
+  nav3.addEventListener('click', () => events.changeStyling('nav-3'));
 }
 
 initBindings();
--- static/js/index.js
+++ static/js/index.js
@@ -2,10 +2,22 @@ const events = require('./events');
 
 function initBindings() {
   let header1 = document.getElementById('heading-1');
+  let heading2 = document.getElementById('heading-2');
+  let nav1 = document.getElementById('nav-1');
+  let nav2 = document.getElementById('nav-2');
+  let nav3 = document.getElementById('nav-3');
   console.log(header1);
 
   header1.addEventListener('click', events.switchColor);
   header1.addEventListener('mouseout', events.returnColor);
+
+  heading2.addEventListener('mouseover', events.changeWords);
+
+  document.addEventListener('keydown', events.addParagraph);
+
+  nav1.addEventListener('click', () => events.changeStyling('nav-1'));
+  nav2.addEventListener('click', () => events.changeStyling('nav-2'));
+  nav3.addEventListener('click', () => events.changeStyling('nav-3'));
 }
 
 initBindings();
